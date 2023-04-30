79ef3a26cbdafa4dd653c7567c13cc19ba38b022
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-04-30 14:26:25 -0400

    more dynamic

--- src/App.js
+++ src/App.js
@@ -9,28 +9,34 @@
 // }
 
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
+  const title = "Blog Post";
+  const body = "This is my blog post";
   const comments = [
-    {id: 1, text: 'comment one'},
-    {id: 2, text: 'comment two'},
-    {id: 3, text: 'comment three'},
-  ]
+    { id: 1, text: "comment one" },
+    { id: 2, text: "comment two" },
+    { id: 3, text: "comment three" },
+  ];
+
+  const loading = false;
+  const showComments = false;
+
+  if (loading) return <h1>Loading...</h1>;
 
   return (
     <div className="container">
       <h1>{title}</h1>
       <p>{body}</p>
-      <div className="comments">
-        <h3>Comments ({comments.length
-          })</h3>
+
+      {showComments && (
+        <div className="comments">
+          <h3>Comments ({comments.length})</h3>
           <ul>
-            {comments.map((comment, index) =>(
-              <li>{comment.text}</li>
+            {comments.map((comment, index) => (
+              <li key={index}>{comment.text}</li>
             ))}
           </ul>
-        
-      </div>
+        </div>
+      )}
     </div>
   );
 }
