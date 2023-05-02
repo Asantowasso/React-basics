9def77c46ed4a5c81e97dac7beecff56e7b66449
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-05-01 22:01:37 -0400

    created Header component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,52 @@
 // Writing functions in this file will allow us to write JSX meaning we can use HTML syntax in our JavaScript
+import Header from "./Components/Header"
+
+function App() {
+  return (
+    <>
+    <Header  />
+    <div className="container">
+      <h1>My App</h1>
+    </div>
+    </>
+  );
+}
+
+
+
+
 
 // function App() {
+//   const title = "Blog Post";
+//   const body = "This is my blog post";
+//   const comments = [
+//     { id: 1, text: "comment one" },
+//     { id: 2, text: "comment two" },
+//     { id: 3, text: "comment three" },
+//   ];
+
+//   const loading = false;
+//   const showComments = false;
+
+//   if (loading) return <h1>Loading...</h1>;
+
 //   return (
 //     <div className="container">
-//       <h1>My App</h1>
+//       <h1>{title}</h1>
+//       <p>{body}</p>
+
+//       {showComments && (
+//         <div className="comments">
+//           <h3>Comments ({comments.length})</h3>
+//           <ul>
+//             {comments.map((comment, index) => (
+//               <li key={index}>{comment.text}</li>
+//             ))}
+//           </ul>
+//         </div>
+//       )}
 //     </div>
 //   );
 // }
-
-function App() {
-  const title = "Blog Post";
-  const body = "This is my blog post";
-  const comments = [
-    { id: 1, text: "comment one" },
-    { id: 2, text: "comment two" },
-    { id: 3, text: "comment three" },
-  ];
-
-  const loading = false;
-  const showComments = false;
-
-  if (loading) return <h1>Loading...</h1>;
-
-  return (
-    <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      {showComments && (
-        <div className="comments">
-          <h3>Comments ({comments.length})</h3>
-          <ul>
-            {comments.map((comment, index) => (
-              <li key={index}>{comment.text}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
-}
 
 export default App;
