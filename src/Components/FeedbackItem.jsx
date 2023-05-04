c4e43397ba46130febd440d7dc5a26590e753e39
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-05-03 21:31:32 -0400

    created basis for ratings

--- src/Components/FeedbackItem.jsx
+++ src/Components/FeedbackItem.jsx
@@ -1,11 +1,20 @@
+
+import {useState} from 'react'
+
+
 function FeedbackItem() {
+  const [rating, setRating] = useState(7)
+  const [text, setText] = useState('Here is an example of a feedback item')
+
+  
+   
 
   return (
     <div className = 'card'>
-      <div className="num-display">10</div>
-    <div className="text-display">
-      Here is an example of a feedback item
-    </div>
+      <div className="num-display">{rating}</div>
+    <div className="text-display">{text}</div>
+    
+    
     </div>
   )
 } 
