2b57e0defc1961a0ce28e258149f423c27db803a
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-05-09 21:56:45 -0400

    added animations when adding and removing reviews

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -1,4 +1,6 @@
 
+import {motion, AnimatePresence} from 'framer-motion'
+
 import FeedbackItem from "./FeedbackItem";
 
 import PropTypes from 'prop-types'
@@ -8,14 +10,29 @@ function FeedbackList({ feedback, handleDelete  }) {
   if(!feedback || feedback.length === 0) {
     return <p>No feedback Yet</p>
   }
-  console.log(feedback);
-  return <div className = 'feedback-list'>
+ 
+   return <div className = 'feedback-list'>
+    <AnimatePresence>
     {feedback.map((item)=> (
+      <motion.div 
+      key={item.id}
+      initial={{opacity: 0}}
+      animate = {{opacity: 1}}
+      exit={{opacity: 0}}
+      >
      <FeedbackItem key={item.id} item={item}
-     handleDelete={handleDelete}
-     />
+     handleDelete={handleDelete} />
+     </motion.div>
     ))}
+    </AnimatePresence>
   </div>;
+  // return <div className = 'feedback-list'>
+  //   {feedback.map((item)=> (
+  //    <FeedbackItem key={item.id} item={item}
+  //    handleDelete={handleDelete}
+  //    />
+  //   ))}
+  // </div>;
 }
 
 FeedbackList.propTypes = {
