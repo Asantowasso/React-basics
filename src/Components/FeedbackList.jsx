16dd25d67d70bb9945d6fbf9ec6f0954a22f34fe
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-05-16 22:20:49 -0400

    eliminated more props

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -1,35 +1,37 @@
+import { motion, AnimatePresence } from "framer-motion";
 
-import {motion, AnimatePresence} from 'framer-motion'
-
-import {useContext} from 'react'
+import { useContext } from "react";
 
 import FeedbackItem from "./FeedbackItem";
 
+import FeedbackContext from "../context/FeedbackContext";
 
-
-import FeedbackContext from '../context/FeedbackContext'
-
-function FeedbackList({handleDelete}) {
-  const {feedback} = useContext(FeedbackContext)
-  if(!feedback || feedback.length === 0) {
-    return <p>No feedback Yet</p>
+function FeedbackList({ handleDelete }) {
+  const { feedback } = useContext(FeedbackContext);
+  if (!feedback || feedback.length === 0) {
+    return <p>No feedback Yet</p>;
   }
- 
-   return <div className = 'feedback-list'>
-    <AnimatePresence>
-    {feedback.map((item)=> (
-      <motion.div 
-      key={item.id}
-      initial={{opacity: 0}}
-      animate = {{opacity: 1}}
-      exit={{opacity: 0}}
-      >
-     <FeedbackItem key={item.id} item={item}
-     handleDelete={handleDelete} />
-     </motion.div>
-    ))}
-    </AnimatePresence>
-  </div>;
+
+  return (
+    <div className="feedback-list">
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+  );
   // return <div className = 'feedback-list'>
   //   {feedback.map((item)=> (
   //    <FeedbackItem key={item.id} item={item}
@@ -38,7 +40,5 @@ function FeedbackList({handleDelete}) {
   //   ))}
   // </div>;
 }
-
-
 
 export default FeedbackList;
--- src/Components/FeedbackStats.jsx
+++ src/Components/FeedbackStats.jsx
@@ -1,6 +1,10 @@
-import PropTypes from 'prop-types'
 
-function FeedbackStats({feedback}) {
+import {useContext} from 'react'
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext)
+
 let average = feedback.reduce((acc, cur) => {
   return acc + cur.rating
 }, 0) / feedback.length
@@ -17,9 +21,7 @@ average = average.toFixed(1).replace(/[.,]0$/,'')
   
   
 }
-FeedbackStats.propTypes = {
-  feedback: PropTypes.array.isRequired,
-}
+
 
 
 export default FeedbackStats
