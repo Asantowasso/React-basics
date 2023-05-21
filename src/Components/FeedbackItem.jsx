8cfbc2e2469116b5c41141066c15475aad19f779
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-05-21 14:08:24 -0400

    added edit button

--- src/Components/FeedbackItem.jsx
+++ src/Components/FeedbackItem.jsx
@@ -1,4 +1,4 @@
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEdit } from "react-icons/fa";
 import {useContext} from 'react'
 
 import Card from "./shared/Card";
@@ -15,7 +15,9 @@ function FeedbackItem({ item, }) {
       <button onClick = {() => deleteFeedback(item.id)} className="close">
         <FaTimes color="purple" />
       </button>
-
+    <button className="edit">
+      <FaEdit color='purple' />
+    </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
