5a2323a4f0ed4c0b1d638bea90b21ec1b005274c
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-05-31 22:38:13 -0400

    using state to display feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,34 +1,32 @@
 import { v4 as uuidv4 } from "uuid";
 
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This is the first feedback item",
-      rating: 10,
-    },
-
-    {
-      id: 2,
-      text: "This is the second feedback item",
-      rating: 7,
-    },
-
-    {
-      id: 3,
-      text: "This is the third feedback item",
-      rating: 4,
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState (true)
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch our feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    );
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false)
+  };
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -42,8 +40,8 @@ export const FeedbackProvider = ({ children }) => {
   };
   const updateFeedback = (id, updItem) => {
     setFeedback(
-      feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
-    
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
   };
 
   //This is what allows the item to be updated
@@ -63,6 +61,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
