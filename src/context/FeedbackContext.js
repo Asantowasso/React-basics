4824dd8ddae5538f4e052d7e94b065ce35c65438
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-06-04 13:12:47 -0400

    able to dedit and remove posts

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -43,14 +43,26 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([data, ...feedback]);
   };
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("are you sure?")) {
+      await fetch (`/feedback/${id}`, {method: 'DELETE'})
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch (`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem)
+
+    })
+
+    const data = await response.json()
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
